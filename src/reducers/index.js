a49f40ae185dfbb287b61ebe32077a969d8cf61f
kujo-kun <kazuya.kujo2@gmail.com>
2022-04-14 20:17:19 +0100

    Commiting project to github

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import PostReducer from "./PostReducer";
+import usersReducer from "./usersReducer";
+export default combineReducers({
+  posts: PostReducer,
+  users: usersReducer,
+});
